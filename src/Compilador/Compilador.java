b9576c0469029ef0167e50208fdb5117fc33f81f
Leandro <leandro.david.dini@gmail.com>
2022-09-15 00:20:30 -0300

    modificaciones de inicio de la clase compilador

--- src/Compilador/Compilador.java
+++ src/Compilador/Compilador.java
@@ -1,10 +1,37 @@
 package Compilador;
 
+import java.util.Scanner;
+import java.io.*;
+
+
 public class Compilador {
 
-	public static void main(String[] args) {
-		System.out.print("hola");
 
+	
+	public static void main(String[] args) {
+        try{
+            //Abro stream, crea el fichero si no existe
+            FileWriter fw=new FileWriter("\fichero1.txt");
+            //Escribimos en el fichero un String y un caracter 97 (a)
+            fw.write("Esto es una prueb");
+            fw.write(97);
+            //Cierro el stream
+            fw.close(); 
+                //Abro el stream, el fichero debe existir
+            FileReader fr=new FileReader("\fichero1.txt");
+            //Leemos el fichero y lo mostramos por pantalla
+            int valor=fr.read();
+            while(valor!=-1){
+                System.out.print((char)valor);
+                valor=fr.read();
+            }
+            //Cerramos el stream
+            fr.close();
+        }catch(IOException e){
+            System.out.println("Error E/S: "+e);
+        }
+    }
+		
 	}
 
 }
